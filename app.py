cf1a73329955cf505ca89f6cf6e184d6f5f2fa88
Vishal Naik <naikvishal12.nv@gmail.com>
2022-04-07 19:07:27 -0700

    attendence database integrated

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot.trainers import ListTrainer
 from train import *
 from filterquestions import *
+from getData import *
 
 
 app = Flask(__name__)
@@ -15,10 +16,13 @@ File_Handler=FileHandler('errorlog.txt')
 File_Handler.setLevel(WARNING)
 #create chatbot
 print("Started...")
-chatbot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+chatbot = ChatBot("Sisly Chatbot", storage_adapter="chatterbot.storage.SQLStorageAdapter", read_only=True,logic_adapter=["chatterbot.logic.BestMatch",'chatterbot.logic.MathematicalEvaluation','chatterbot.logic.TimeLogicAdapter'],preprocessors=['chatterbot.preprocessors.clean_whitespace'])
 trainer = ChatterBotCorpusTrainer(chatbot)
 #trainer.train("chatterbot.corpus.english") #train the chatter bot for english
-
+'''def datat(d):
+    a=get_Attendence(d)
+    print(a+"yuyu")
+datat(39)'''
 trainer1 = ListTrainer(chatbot)
 trainer1.train(data)
 trainer1.train(data1)
@@ -33,10 +37,15 @@ def index():
 #function for the bot response
 def get_bot_response():
     userText = request.args.get('msg')
-    if userText not in filter:
-        return str("Sorry not in database!")
+
     if userText[:8] == "rollno::":
-        return str("yes done")
+        RollNo=userText[8:]
+        print(RollNo)
+        attendence=get_Attendence(RollNo)
+        return str(attendence)
+    elif userText not in filter:
+        return str("Sorry not in database!")
+
     bot_response=chatbot.get_response(userText)
     return str(bot_response)
 
